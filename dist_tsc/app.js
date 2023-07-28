cefa81de88e440d90f8310f96951c15e725653bb
Gustavo Henrique <Gustavo326projetos@gmail.com>
2023-07-28 20:11:35 -0300

    Cors

--- dist_tsc/app.js
+++ dist_tsc/app.js
@@ -4,14 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const server_1 = require("./server/server");
 const app = (0, express_1.default)();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    app.use((0, cors_1.default)());
-    next();
+    const allowedOrigin = 'http://localhost:8080';
+    if (req.headers.origin === allowedOrigin) {
+        res.header('Access-Control-Allow-Origin', allowedOrigin);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
+    else {
+        return res.status(403).json({ error: 'Acesso negado.' });
+    }
 });
 app.use(server_1.server);
 app.listen(8080, () => console.log('Servidor Aguardando requisições na porta 8080'));
