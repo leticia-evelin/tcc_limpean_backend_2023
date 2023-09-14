1bd35ff045d26e756beedac0c9610cac4f506c32
Gustavo Henrique <109995734+GustavoHenriqueProjects@users.noreply.github.com>
2023-09-14 11:17:34 -0300

    Atualizado

--- src/controller/controllerDiarista/updateDataPersonalDiarist/controllerUpdateDataPersonalDiarist.ts
+++ src/controller/controllerDiarista/updateDataPersonalDiarist/controllerUpdateDataPersonalDiarist.ts
@@ -17,22 +17,22 @@ interface TokenPayLoad {
 }
 
 const updateDataDiarist = async function (token:string, dataDiarist: UpdateDataDiarist) {
-    // const SECRETE = '3oFEe4PtHxJeXsa7hY8WBFtCt1AJ4GwgqF6WARF1NG0mUnc89W'
+    const SECRETE = message.REQUIRE_SECRETE
 
-    // try {
+    try {
         
-    //     const decoded = jwt.verify(Array.isArray(token) ? token[0] : token, SECRETE) as TokenPayLoad
-    //     const { id, name} = decoded 
+        const decoded = jwt.verify(Array.isArray(token) ? token[0] : token, SECRETE) as TokenPayLoad
+        const { id, name} = decoded 
 
         
-    //     const statusDiarist = await deleteDiarist(Number(id), name)
-    //     if(statusDiarist){
-    //         return message.DELETE_USER
-    //     }else{
-    //         return message.ERRO_DELETE_USER
-    //     }
+        // const statusDiarist = await deleteDiarist(Number(id), name)
+        // if(statusDiarist){
+        //     return message.DELETE_USER
+        // }else{
+        //     return message.ERRO_DELETE_USER
+        // }
 
-    // } catch (error) {
-    //     return message.ERRO_INVALID_TOKEN
-    // }
+    } catch (error) {
+        return message.ERRO_INVALID_TOKEN
+    }
 }
